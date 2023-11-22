20283a8c1fbfe40a51207960df4c215bf4fcacac
coutJP <jeanpaul_mansour@outlook.com>
2023-11-22 18:04:50 +0200

    last edits

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import './NavBar.css';
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import logo from './logo.png'
+import { GiDreamCatcher } from "react-icons/gi";
 
 const Navbar = () => {
     const handleInstagramClick = () => {
@@ -10,12 +12,18 @@ const Navbar = () => {
       const handleWhatsappClick = () => {
         window.open('https://wa.me/9613108087', '_blank');
       };
+      const logoclick = () => {
+        window.location.href = '/';
+      };
   return (
     
     <nav className="navbar">
       <div className="left">
-        <h1>KharzatinaByR</h1>
+        <h1 style={{cursor:'pointer',fontFamily: 'Aguafina Script',fontSize: '22px'}} onClick={logoclick}><GiDreamCatcher style={{fontSize:'40px'}}/> KharzatinaByR</h1>
       </div>
+      {/* <div className="middle">
+        <img src={logo} alt="Logo" style={{ width: '50px', height: '50px', borderRadius: '50%' }} />
+      </div> */}
       <div className="right">
         <p>Contact Us:</p>
         <a className='bt'> <FaInstagram onClick={handleInstagramClick} /> </a>
